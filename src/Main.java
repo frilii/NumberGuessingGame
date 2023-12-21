cbc9c15ac463f36ac120024f2720ffc1280c10cd
frilii <gualbertofabricio4@gmail.com>
2023-12-21 10:56:19 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
-        int number = random.nextInt(100); // Generate random number between 0 and 99
+        int number = random.nextInt(100); 
 
         Scanner sc = new Scanner(System.in);
 
-        boolean guessed = false; // Flag to track if the number is guessed
+        boolean guessed = false; 
 
-        while (!guessed) { // Keep looping until the number is guessed
-            System.out.println("Guess a number between 0 and 99:");
+        while (!guessed) {
+            System.out.println("Adivinhe um número entre 0 e 99:");
             int guess = sc.nextInt();
 
             if (guess == number) {
-                System.out.println("Congratulations, you guessed it!");
-                guessed = true; // Set flag to true to exit the loop
+                System.out.println("Parabéns, você acertou!");
+                guessed = true; 
             } else if (guess < number) {
-                System.out.println("Too low!");
+                System.out.println("Muito baixo!");
             } else {
-                System.out.println("Too high!");
+                System.out.println("Muito alto!");
             }
         }
 
-        sc.close(); // Close the scanner to avoid resource leaks
+        sc.close(); 
     }
 }
